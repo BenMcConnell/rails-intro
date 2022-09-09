4d3db8791bc62825db4ab16ff2dfe4460ded3581
Benjamin McConnell <crosslegged@gmail.com>
2022-09-09 10:52:53 +0200

    controller & pages updated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,11 @@ class PagesController < ApplicationController
 
   def contact
     @members = ["Ben", "Orso", "Virginie", "Jude"]
+
+    search = params[:member]
+    if search.present?
+      @members = @members.select { |member| member.downcase.include?(search.downcase) }
+    end
   end
 
   def home
